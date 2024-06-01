3ee2cca86f06be828cefb23e20f3cbcfd6b8cd00
Amaddied <amandakss25@gmail.com>
2024-06-01 12:10:04 -0300

    dark theme e cor roxa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import { unstable_createMuiStrictModeTheme } from "@mui/material";
+import { createTheme } from "@mui/material";
+import React from "react";
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#9c27b0',
+    },
+    secondary: {
+      main: '#af52bf',
+    },
+  },
+})
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
-    </div>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <Header />
+          <ListarTarefa />
+        </div>
+        <CssBaseline />
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
 
